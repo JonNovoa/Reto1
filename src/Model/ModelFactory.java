6f20e7378107441ad3671844ca16e32c469bfaeb
jon <jon.novoa.alaiz@gmail.com>
2022-09-21 08:24:07 +0200

    PaquetesYClases

--- src/Model/ModelFactory.java
+++ src/Model/ModelFactory.java
@@ -3,4 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto0din;
+package Model;
+
+/**
+ *
+ * @author somor
+ */
+public class ModelFactory {
+    
+}
--- src/View/ViewInterface.java
+++ src/View/ViewInterface.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto0din;
+package View;
 
 /**
  *
  * @author somor
  */
-public class Reto0DIN {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public interface ViewInterface {
     
 }
